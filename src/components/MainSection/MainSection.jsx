8871ba2b9919811e70f11372b1a9cc71e43e55d6
codingisfun <61808280+jennyq7@users.noreply.github.com>
2023-05-28 22:27:22 -0400

    fixed bugs for form, styling result

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -9,26 +9,26 @@ function MainSection () {
     function handleSubmit (e) {
         e.preventDefault();
 
-        const ounceNum = e.target.ounce.value;
-         console.log(e.target.ounce);
+        const ounceNum = Number(e.target.ounce.value);
+        
         if (typeof ounceNum === "number") {
             setResult(ounceNum * 29.574);
-        }
+        } 
         else {return "Please enter a number in the box above."}
-
+        
         
     }
 
-
+    
     return (
         <main className="main">
            <h3 className="main__text">Use the form to get your conversion!</h3> 
-           <form className="main__form">
+           <form className="main__form" onSubmit={handleSubmit}>
               <label className="main__form-ounce-label" htmlFor="ounce">Ounce</label>
               <input className="main__form-ounce-field" type="number" name="ounce" id="ounce" placeholder="12" required />
-              <button className="main__form-button" onClick={handleSubmit}>Convert!</button>
-              <label className="main__form-mill-label" htmlFor="mill">Millimeters</label>
-              <span>{result}</span>        
+              <button className="main__form-button">Convert!</button>
+              <label className="main__form-mill-label">Millimeters</label>
+              <span className="main__form-result">{result}</span>        
            </form>
         </main>
     )
